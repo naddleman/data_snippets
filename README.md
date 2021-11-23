6959f931462f5082daf5100cbaae5b6337fa3fb2
Nikhil Addleman <nik.addleman@gmail.com>
2021-11-23 13:08:53 -0500

    Kernel smoothing

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
+# snippets
+
 Little Python code snippets to demo various data analysis techniques
+
+## Kernel smoothing
+
+Implementing k-nearest neighbors (KNN) interpolation estimates a function.
+
+Observe the estimated functions are jagged, with discontinuities whenever
+a new point becomes one of the k used in calculating the average.
+
+The discontinuities can be improved by kernel smoothing. In this case a
+weighted average is taken of all points in a window of radius lambda.
+
+The weights are given by a kernel function that decreases to zero at the
+edges of the window, and so the estimated value at a point x_0 is
+
+f(x_0) = (sum_{i} K(x_0, x_i) * y_i) / sum K(x_0, x_i)
+
+In words, the prediction at x_0 is a weighted average of nearby observations.
+
+A drawback of these local smoothing techniques is poor predictions at the
+boundaries. Observe that all of these methods overestimate the function on
+the left edge. In this region the function is increasing, and because it is
+a boundary, the points that contribute to the average are all taken from
+one side.
+
+
--- kernel_smoothing.py
+++ kernel_smoothing.py
@@ -17,21 +17,51 @@ Y_observed = f(X) + eps
 unit_interval = np.linspace(0,1,1001)
 Y_true = f(unit_interval)
 
-plt.plot(unit_interval, Y_true, c='red', label="True values")
-plt.scatter(X, Y_observed, marker='.', c='green', label="Observations")
-ax = plt.gca()
 
 # Generate a matrix of distances?
-distances = abs(unit_interval[:,np.newaxis] - X)
+def dmatrix(linspace, obs_points):
+    return abs(linspace[:,np.newaxis] - obs_points)
+
+#distances = abs(unit_interval[:,np.newaxis] - X)
 def knn(linspace, X, observations, k=10):
-    distance_matrix = abs(linspace[:,np.newaxis] - X)
+    #distance_matrix = abs(linspace[:,np.newaxis] - X)
+    distance_matrix = dmatrix(linspace, X)
     nearest_neighbors = np.argpartition(distance_matrix, k)[:,:k]
     knn_vals = observations[nearest_neighbors]
     knn_means = np.mean(knn_vals, axis=1)
     return knn_means
 
 knn_regr = knn(unit_interval, X, Y_observed, k=10)
-plt.plot(unit_interval, knn_regr, c='blue', label="K=10 nearest neighbors")
+knn_regr30 = knn(unit_interval, X, Y_observed, k=30)
 
-ax.legend()
+def epanechnikov(linspace, X, observations, lam=0.2):
+    """From Elements of Statistical Learning, pg 193"""
+    def D(mat):
+        out_matrix = np.empty(mat.shape)
+        mask = mat > 1
+        out_matrix = np.where(mask, 0, 3/4 * (1-mat**2))
+        return out_matrix
+    distances = dmatrix(linspace, X)
+    K = D(distances / lam)
+    val = (K * Y_observed).sum(axis=1)
+    normalizer = K.sum(axis=1)
+    return val / normalizer
+
+epan_lam_2 = epanechnikov(unit_interval, X, Y_observed)
+epan_lam_1 = epanechnikov(unit_interval, X, Y_observed, lam=0.1)
+
+fig, axs = plt.subplots(2)
+for ax in axs:
+    ax.plot(unit_interval, Y_true, c='red', label="True values")
+    ax.scatter(X, Y_observed, marker='.', c='green', label="Observations")
+
+axs[0].plot(unit_interval, knn_regr, c='blue', label="K=10 nearest neighbors")
+axs[0].plot(unit_interval, knn_regr30, c='orange', label="K=30 nearest neighbors")
+
+axs[1].plot(unit_interval, epan_lam_2, c='blue', label="Epanechnikov kernel λ=.2")
+axs[1].plot(unit_interval, epan_lam_1, c='orange', label="Epanechnikov kernel λ=.1")
+
+for ax in axs:
+    ax.legend()
+
 plt.show()
